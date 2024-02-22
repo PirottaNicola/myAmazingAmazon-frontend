773de39aada0af0b116d82cdda131fc98a5a7028
Nicola Pirotta <nicolapirotta@gmail.com>
2024-02-22 15:09:39 +0100

    aggiunte root variables con colori e font dell'applicazione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomePageComponent } from './views/homepage/homepage.component';
 import { CartComponent } from './views/cart/cart.component';
-import { OrdersComponent } from './views/orders/orders.component';
 import { ErrorComponent } from './views/error/error.component';
+import { HomePageComponent } from './views/homepage/homepage.component';
+import { OrdersComponent } from './views/orders/orders.component';
 
 @NgModule({
   // The declarations array is used to define the components, directives, and pipes that are used in the application
-  declarations: [AppComponent, NavbarComponent, HomePageComponent, CartComponent, OrdersComponent, ErrorComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    CartComponent,
+    OrdersComponent,
+    ErrorComponent,
+  ],
   // The imports array is used to import other modules into the application
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule, // The BrowserModule module is used to import the basic directives and services that are used to run the application in the browser
+    AppRoutingModule, // The AppRoutingModule module is used to define the routes for the application
+  ],
   // The providers array is used to define the services that are used in the application
   providers: [],
   // The bootstrap array is used to define the root component of the application
